46e3b8e64de793b09d006cd7ad3737865f7411d8
Emily Hyland <hello@duien.com>
2019-06-08 02:06:50 -0700

    Let's take a look at the headers

--- lib/dashboard_experiment_web/controllers/webhook_controller.ex
+++ lib/dashboard_experiment_web/controllers/webhook_controller.ex
@@ -1,7 +1,18 @@
 defmodule DashboardExperimentWeb.WebhookController do
   use DashboardExperimentWeb, :controller
 
+  plug :validate_github_token when action in [:github]
+
   def github(conn, _params) do
-    json(conn, %{hello: "world"})
+    json(conn, conn.req_headers)
+  end
+
+  defp validate_github_token(conn, _) do
+    # IO.inspect(conn.req_headers)
+    # conn
+    # |> get_req_header('x_hub_signature')
+
+
+    conn
   end
 end

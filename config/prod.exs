356b6d7bbb808fbc8df53172729334fb2c11cc9a
Emily Hyland <hello@duien.com>
2019-06-08 01:17:02 -0700

    Configure for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dashboard_experiment, DashboardExperimentWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "fierce-sierra-97080", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
